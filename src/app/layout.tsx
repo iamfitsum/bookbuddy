d4bff2633692db291e14306776833f7f5c1a0a62
Fitsum Mekonnen <official.fitsum.mekonnen@gmail.com>
2023-05-30 02:24:39 +0300

    ChatGPT Message done with the data it needs + prompt to make it helpful bookstore assistant

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Chat from "@/components/Chat";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Chat />
-        {children}
-      </body>
+      <Providers>
+        <body className={inter.className}>
+          <Chat />
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
