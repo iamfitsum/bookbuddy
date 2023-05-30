1f3767eb40997ae27ff264c8d083cc0ed95bd1b4
Fitsum Mekonnen <official.fitsum.mekonnen@gmail.com>
2023-05-31 00:14:13 +0300

    Finishing touches + Code Cleanup

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,12 +2,16 @@
 
 import { MessagesProvider } from "@/context/messages";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <MessagesProvider>{children}</MessagesProvider>
+      <MessagesProvider>
+        <Toaster position="top-center" />
+        {children}
+      </MessagesProvider>
     </QueryClientProvider>
   );
 };
